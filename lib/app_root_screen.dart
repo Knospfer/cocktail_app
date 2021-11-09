ffe2b4453db3c013ca3a377ac74c119a3d2157c4
knospfer <berrueta.mattia@gmail.com>
2021-11-09 20:20:08 +0100

    feat: app tab bar in root screen

--- lib/app_root_screen.dart
+++ lib/app_root_screen.dart
@@ -1,9 +1,8 @@
+import 'package:cocktail_app/_shared/widgets/app_tab_bar.dart';
 import 'package:cocktail_app/screens/favourites.dart';
 import 'package:cocktail_app/screens/home.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
-import '_shared/widgets/circle_indicator.dart';
 
 class AppRootScreen extends StatelessWidget {
   const AppRootScreen({Key? key}) : super(key: key);
@@ -22,15 +21,19 @@ class AppRootScreen extends StatelessWidget {
               FavouritesScreen(),
             ],
           ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {},
-            backgroundColor: Colors.pink,
-            child: const Icon(Icons.qr_code),
+          floatingActionButton: Container(
+            alignment: Alignment.bottomCenter,
+            height: 80,
+            child: FloatingActionButton(
+              onPressed: () {},
+              backgroundColor: Colors.blueGrey,
+              child: const Icon(Icons.qr_code),
+              elevation: 1,
+            ),
           ),
           floatingActionButtonLocation:
               FloatingActionButtonLocation.centerDocked,
-          bottomNavigationBar: const TabBar(
-            indicator: CircleTabIndicator(color: Colors.white, radius: 3),
+          bottomNavigationBar: const AppTabBar(
             tabs: [
               Tab(icon: Icon(Icons.home)),
               Tab(icon: Icon(Icons.favorite)),
