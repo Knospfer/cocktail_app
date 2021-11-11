3ed0916c34816521eddd9e6720a52a7a628d1a8a
knospfer <berrueta.mattia@gmail.com>
2021-11-11 07:59:23 +0100

    feat: app initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dependency_injection.dart';
 import 'app.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  
+  await configureDependencies();
   runApp(const App());
 }
