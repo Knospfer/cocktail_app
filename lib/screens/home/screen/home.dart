48c795fdb448649424cf16b11d7c04f8bb0c0488
knospfer <berrueta.mattia@gmail.com>
2021-11-11 08:21:08 +0100

    feat: home screen with viewmodel

--- lib/screens/home/screen/home.dart
+++ lib/screens/home/screen/home.dart
@@ -1,13 +1,34 @@
+import 'package:cocktail_app/screens/home/view_model/home_view_model.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void didChangeDependencies() {
+    Provider.of<HomeViewModel>(context).fetchCocktailList();
+    super.didChangeDependencies();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text("Home!"),
+        child: Consumer<HomeViewModel>(
+          builder: (context, viewModel, child) {
+            return ListView(
+              children: viewModel.cocktails
+                  .map((c) => ListTile(title: Text(c.name)))
+                  .toList(),
+            );
+          },
+        ),
       ),
     );
   }
