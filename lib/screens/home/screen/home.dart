8a6bb6b315e7b5b7f4899592275b5cb42c6c8fb2
knospfer <berrueta.mattia@gmail.com>
2021-11-11 19:09:32 +0100

    feat: home screen initial version

--- lib/screens/home/screen/home.dart
+++ lib/screens/home/screen/home.dart
@@ -19,13 +19,72 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      body: Container(
+        padding: const EdgeInsets.only(
+          top: 24,
+          left: 24,
+          right: 24,
+        ),
+        alignment: Alignment.center,
         child: Consumer<HomeViewModel>(
           builder: (context, viewModel, child) {
             return ListView(
-              children: viewModel.cocktails
-                  .map((c) => ListTile(title: Text(c.name)))
-                  .toList(),
+              physics: const BouncingScrollPhysics(),
+              children: [
+                const Text(
+                  "Hi",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                  ),
+                ),
+                const Padding(padding: EdgeInsets.only(bottom: 10)),
+                const Text(
+                  "What would you like \nto drink tonight?",
+                  style: TextStyle(color: Colors.white, fontSize: 24),
+                ),
+                const Padding(padding: EdgeInsets.only(bottom: 20)),
+                ...viewModel.cocktails
+                    .map(
+                      (c) => Container(
+                        height: 200,
+                        margin: const EdgeInsets.only(bottom: 20),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: NetworkImage(c.image ?? ""),
+                          ),
+                        ),
+                        child: Container(
+                          color: Colors.black26,
+                          padding: const EdgeInsets.all(20),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                c.name,
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                c.category,
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    )
+                    .toList(),
+              ],
             );
           },
         ),
