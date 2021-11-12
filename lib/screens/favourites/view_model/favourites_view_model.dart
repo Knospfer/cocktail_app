aecfbda5a98d02330a8d9cab3757785e906f0596
knospfer <berrueta.mattia@gmail.com>
2021-11-12 19:10:41 +0100

    refactor: removed unused super class

--- lib/screens/favourites/view_model/favourites_view_model.dart
+++ lib/screens/favourites/view_model/favourites_view_model.dart
@@ -1,15 +1,16 @@
 import 'package:cocktail_app/_domain/cocktail/entity/cocktail_entity.dart';
 import 'package:cocktail_app/_domain/cocktail/presentation/cocktail_favourite_handler.dart';
 import 'package:cocktail_app/_domain/cocktail/services/favourite_cocktail.service.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:injectable/injectable.dart';
 
 @injectable
-class FavouritesViewModel extends CocktailFavouriteHandler {
+class FavouritesViewModel extends ChangeNotifier {
   final FavouriteCocktailService _favouriteCocktailService;
+
   List<CocktailEntity> cocktails = [];
 
-  FavouritesViewModel(this._favouriteCocktailService)
-      : super(_favouriteCocktailService);
+  FavouritesViewModel(this._favouriteCocktailService);
 
   Future<void> fetchFavourites() async {
     cocktails = await _favouriteCocktailService.fetchFavourites();
@@ -17,7 +18,6 @@ class FavouritesViewModel extends CocktailFavouriteHandler {
   }
 
   Future<void> removeFromFavourites(CocktailEntity cocktail) async {
-    //TODO MIGIORA MIXIN
     await _favouriteCocktailService.removeFromFavourites(cocktail);
     cocktails
         .removeWhere((current) => current.id == cocktail.id); //TODO EQUATABLE
