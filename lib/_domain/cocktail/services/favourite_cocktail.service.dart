b795264d18c2c745ffb8a777dcb4574cde045038
knospfer <berrueta.mattia@gmail.com>
2021-11-12 11:05:34 +0100

    feat: remove from favourites

--- lib/_domain/cocktail/services/favourite_cocktail.service.dart
+++ lib/_domain/cocktail/services/favourite_cocktail.service.dart
@@ -1,10 +1,7 @@
-
 import 'package:cocktail_app/_domain/cocktail/entity/cocktail_entity.dart';
 import 'package:cocktail_app/_domain/cocktail/store/cocktail.store.dart';
 import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
-
-
 
 @lazySingleton
 @immutable
@@ -13,11 +10,15 @@ class FavouriteCocktailService {
 
   const FavouriteCocktailService(this._favouriteStore);
 
+  Future<List<CocktailEntity>> fetchFavourites() async => _favouriteStore.get();
+
   Future<void> addToFavourites(CocktailEntity cocktail) async {
     cocktail.favourite = true;
     _favouriteStore.add(cocktail);
   }
 
-  Future<List<CocktailEntity>> fetchFavourites() async =>
-      _favouriteStore.get();
+  Future<void> removeFromFavourites(CocktailEntity cocktail) async {
+    cocktail.favourite = false;
+    _favouriteStore.delete(cocktail);
+  }
 }
