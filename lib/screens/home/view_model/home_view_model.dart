0e43ffbf2eed1ddefa6f289d8e0b0962fd310b86
knospfer <berrueta.mattia@gmail.com>
2021-11-12 11:06:14 +0100

    feat: home view model handles favourites

--- lib/screens/home/view_model/home_view_model.dart
+++ lib/screens/home/view_model/home_view_model.dart
@@ -1,14 +1,18 @@
 import 'package:cocktail_app/_domain/cocktail/entity/cocktail_entity.dart';
+import 'package:cocktail_app/_domain/cocktail/presentation/cocktail_favourite_handler.dart';
 import 'package:cocktail_app/_domain/cocktail/services/cocktail.service.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:cocktail_app/_domain/cocktail/services/favourite_cocktail.service.dart';
 import 'package:injectable/injectable.dart';
 
 @injectable
-class HomeViewModel extends ChangeNotifier {
+class HomeViewModel extends CocktailFavouriteHandler {
   final CocktailService _cocktailService;
   List<CocktailEntity> cocktails = [];
 
-  HomeViewModel(this._cocktailService);
+  HomeViewModel(
+    this._cocktailService,
+    FavouriteCocktailService _favouriteCocktailService,
+  ) : super(_favouriteCocktailService);
 
   Future<void> fetchCocktailList() async {
     cocktails = await _cocktailService.fetchCocktailList();
