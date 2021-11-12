d18bbf9cb56e50992a459907c3b42d57d9333d09
knospfer <berrueta.mattia@gmail.com>
2021-11-12 17:52:56 +0100

    refactor: home screen with main screen scaffold

--- lib/screens/home/screen/home.dart
+++ lib/screens/home/screen/home.dart
@@ -1,9 +1,10 @@
 import 'package:cocktail_app/_core/routes/routes.dart';
 import 'package:cocktail_app/_domain/cocktail/entity/cocktail_entity.dart';
 import 'package:cocktail_app/_shared/widgets/cocktail_card.dart';
+import 'package:cocktail_app/_shared/widgets/main_screen_scaffold/main_screen_scaffold.dart';
 import 'package:cocktail_app/_shared/widgets/staggered_sliver_list/staggered_sliver_list.dart';
 import 'package:cocktail_app/screens/home/view_model/home_view_model.dart';
-import 'package:cocktail_app/_shared/widgets/screen_header.dart';
+import 'package:cocktail_app/_shared/widgets/main_screen_scaffold/screen_header.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -45,48 +46,38 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24),
-        child: CustomScrollView(
-          physics: const BouncingScrollPhysics(),
-          slivers: [
-            const SliverToBoxAdapter(
-              child: ScreenHeader(
-                title: "Hey",
-                subtitle: "What would you \nlike to drink tonight?",
-              ),
-            ),
-            Consumer<HomeViewModel>(
-              builder: (context, viewModel, child) {
-                final cocktailList = viewModel.cocktails;
-                
-                if (cocktailList.isNotEmpty) {
-                  _key.currentState?.addItemsStaggered(cocktailList);
-                }
+    return MainScreenScaffold(
+      title: "Hey",
+      subtitle: "What would you \nlike to drink tonight?",
+      children: [
+        Consumer<HomeViewModel>(
+          builder: (context, viewModel, child) {
+            final cocktailList = viewModel.cocktails;
 
-                return StaggeredSliverList<CocktailEntity>(
-                  key: _key,
-                  builder: (cocktail) {
-                    return GestureDetector(
-                      behavior: HitTestBehavior.deferToChild,
-                      onTap: () async {
-                        _navigateToDetail(cocktail);
-                      },
-                      child: CocktailCard(
-                        onFavouriteTapped: () {
-                          _toggleFavourites(cocktail);
-                        },
-                        cocktail: cocktail,
-                      ),
-                    );
+            if (cocktailList.isNotEmpty) {
+              _key.currentState?.addItemsStaggered(cocktailList);
+            }
+
+            return StaggeredSliverList<CocktailEntity>(
+              key: _key,
+              builder: (cocktail) {
+                return GestureDetector(
+                  behavior: HitTestBehavior.deferToChild,
+                  onTap: () async {
+                    _navigateToDetail(cocktail);
                   },
+                  child: CocktailCard(
+                    onFavouriteTapped: () {
+                      _toggleFavourites(cocktail);
+                    },
+                    cocktail: cocktail,
+                  ),
                 );
               },
-            ),
-          ],
+            );
+          },
         ),
-      ),
+      ],
     );
   }
 }
