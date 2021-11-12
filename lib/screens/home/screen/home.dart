22131307bf9af9aefa82351a8effcf9f12c87599
knospfer <berrueta.mattia@gmail.com>
2021-11-12 08:40:57 +0100

    feat: open detail page

--- lib/screens/home/screen/home.dart
+++ lib/screens/home/screen/home.dart
@@ -1,3 +1,4 @@
+import 'package:cocktail_app/_core/routes/routes.dart';
 import 'package:cocktail_app/screens/home/view_model/home_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -46,39 +47,48 @@ class _HomeScreenState extends State<HomeScreen> {
                 const Padding(padding: EdgeInsets.only(bottom: 20)),
                 ...viewModel.cocktails
                     .map(
-                      (c) => Container(
-                        height: 200,
-                        margin: const EdgeInsets.only(bottom: 20),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(8),
-                          image: DecorationImage(
-                            fit: BoxFit.cover,
-                            image: NetworkImage(c.image ?? ""),
-                          ),
-                        ),
+                      (c) => GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(
+                            context,
+                            Routes.detail,
+                            arguments: c,
+                          );
+                        },
                         child: Container(
-                          color: Colors.black26,
-                          padding: const EdgeInsets.all(20),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                c.name,
-                                style: const TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 30,
-                                  fontWeight: FontWeight.bold,
+                          height: 200,
+                          margin: const EdgeInsets.only(bottom: 20),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: NetworkImage(c.image ?? ""),
+                            ),
+                          ),
+                          child: Container(
+                            color: Colors.black26,
+                            padding: const EdgeInsets.all(20),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  c.name,
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.bold,
+                                  ),
                                 ),
-                              ),
-                              Text(
-                                c.category,
-                                style: const TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 20,
+                                Text(
+                                  c.category,
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20,
+                                  ),
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
                         ),
                       ),
