66a56b6155e37793487286e08ea43238944e9886
knospfer <berrueta.mattia@gmail.com>
2021-11-12 19:06:02 +0100

    refactor: core quality

--- lib/screens/home/screen/home.dart
+++ lib/screens/home/screen/home.dart
@@ -4,7 +4,6 @@ import 'package:cocktail_app/_shared/widgets/cocktail_card.dart';
 import 'package:cocktail_app/_shared/widgets/main_screen_scaffold/main_screen_scaffold.dart';
 import 'package:cocktail_app/_shared/widgets/staggered_sliver_list/staggered_sliver_list.dart';
 import 'package:cocktail_app/screens/home/view_model/home_view_model.dart';
-import 'package:cocktail_app/_shared/widgets/main_screen_scaffold/screen_header.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -52,10 +51,10 @@ class _HomeScreenState extends State<HomeScreen> {
       children: [
         Consumer<HomeViewModel>(
           builder: (context, viewModel, child) {
-            final cocktailList = viewModel.cocktails;
+            final cocktails = viewModel.cocktails;
 
-            if (cocktailList.isNotEmpty) {
-              _key.currentState?.addItemsStaggered(cocktailList);
+            if (cocktails.isNotEmpty) {
+              _key.currentState?.addItemsStaggered(cocktails);
             }
 
             return StaggeredSliverList<CocktailEntity>(
@@ -63,7 +62,7 @@ class _HomeScreenState extends State<HomeScreen> {
               builder: (cocktail) {
                 return GestureDetector(
                   behavior: HitTestBehavior.deferToChild,
-                  onTap: () async {
+                  onTap: () {
                     _navigateToDetail(cocktail);
                   },
                   child: CocktailCard(
