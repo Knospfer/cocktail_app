22278e49c730f75cc16bf47647369a2a8c9a3825
knospfer <berrueta.mattia@gmail.com>
2021-11-13 15:32:47 +0100

    feat: update filter state

--- lib/screens/search/view_model/search_bottom_sheet_view_model.dart
+++ lib/screens/search/view_model/search_bottom_sheet_view_model.dart
@@ -1,3 +1,4 @@
+import 'package:cocktail_app/_core/enums/alcohol_presence.dart';
 import 'package:cocktail_app/_domain/filter/filter_entities.dart';
 import 'package:cocktail_app/_domain/filter/filter_service.dart';
 import 'package:flutter/cupertino.dart';
@@ -7,11 +8,25 @@ import 'package:injectable/injectable.dart';
 class SearchBottomSheetViewModel extends ChangeNotifier {
   final FilterService _filterService;
   FilterDataEntity? filterData;
+  ApllyingFilterEntity? applyFilter;
 
   SearchBottomSheetViewModel(this._filterService);
 
   Future<void> createSearchFilter() async {
     filterData = await _filterService.fillFilter();
     notifyListeners();
+  }
+
+  void updateFilter({
+    String? name,
+    String? category,
+    AlcoholPresence? alcoholPresence,
+    List<String>? ingredients,
+  }) {
+    applyFilter ??= ApllyingFilterEntity();
+    if (name != null) applyFilter?.name = name;
+    if (category != null) applyFilter?.category = category;
+    if (alcoholPresence != null) applyFilter?.alcoholPresence = alcoholPresence;
+    if (ingredients != null) applyFilter?.ingredients = ingredients;
   }
 }
