3111fc21c3687bc53429151a12cd9dcb0e20a129
knospfer <berrueta.mattia@gmail.com>
2021-11-13 15:33:05 +0100

    feat: dismiss keyboard

--- lib/screens/search/screen/search_bottom_sheet_screen.dart
+++ lib/screens/search/screen/search_bottom_sheet_screen.dart
@@ -21,46 +21,51 @@ class _SearchBottomSheetScreenState extends State<SearchBottomSheetScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      clipBehavior: Clip.hardEdge,
-      decoration: const BoxDecoration(
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(20),
-          topRight: Radius.circular(20),
-        ),
-      ),
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-          title: const Text("Search"),
-          centerTitle: true,
-          leading: IconButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            icon: const Icon(Icons.close),
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Container(
+        clipBehavior: Clip.hardEdge,
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
           ),
-          actions: [
-            IconButton(
-              onPressed: () {
-                //TODO cerca
-              },
-              icon: const Icon(Icons.search),
-            )
-          ],
         ),
-        body: Consumer<SearchBottomSheetViewModel>(
-          builder: (context, viewModel, child) {
-            final filter = viewModel.filterData;
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            title: const Text("Search"),
+            centerTitle: true,
+            leading: IconButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              icon: const Icon(Icons.close),
+            ),
+            actions: [
+              IconButton(
+                onPressed: () {
+                  //TODO cerca
+                },
+                icon: const Icon(Icons.search),
+              )
+            ],
+          ),
+          body: Consumer<SearchBottomSheetViewModel>(
+            builder: (context, viewModel, child) {
+              final filter = viewModel.filterData;
 
-            if (filter == null) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-            return SearchBottomSheet(filter: filter);
-          },
+              if (filter == null) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              return SearchBottomSheet(filter: filter);
+            },
+          ),
         ),
       ),
     );
