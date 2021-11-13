0293072daf44b99d435d8143ab7aa3edf68c8e38
knospfer <berrueta.mattia@gmail.com>
2021-11-13 12:53:57 +0100

    feat: reverted api service to original state

--- lib/_core/services/api/api.service.dart
+++ lib/_core/services/api/api.service.dart
@@ -9,16 +9,11 @@ class ApiService {
 
   const ApiService(this._client);
 
-  Future<Map<String, dynamic>> getValue(String path) async {
+  Future<Map<String, dynamic>> get(String path) async {
     final response = await _client.get<Map<String, dynamic>>(path);
     final data = response.data;
 
     if (response.statusCode == 200 && data != null) return data;
     throw (Exception("Api call $path failed!"));
-  }
-
-  Future<List> getList(String path) async {
-    final response = await getValue(path);
-    return response.values as List;
   }
 }
