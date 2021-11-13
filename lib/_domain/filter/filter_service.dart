a9cd581765b56d38889ed5ba4911257bf4274d1c
knospfer <berrueta.mattia@gmail.com>
2021-11-13 12:54:49 +0100

    fix: filter service casting

--- lib/_domain/filter/filter_service.dart
+++ lib/_domain/filter/filter_service.dart
@@ -1,4 +1,5 @@
 import 'package:cocktail_app/_core/services/api/api.service.dart';
+import 'package:cocktail_app/_core/services/api/api_response.dart';
 import 'package:cocktail_app/_domain/filter/filter_entities.dart';
 import 'package:cocktail_app/_domain/filter/filters_model.dart';
 import 'package:injectable/injectable.dart';
@@ -11,35 +12,43 @@ class FilterService {
 
   //TODO APPLY FILTER
 
-  Future<FillingFilterEntity> fillFilter() async {
+  Future<FilterDataEntity> fillFilter() async {
     final categories = await _getCategories();
     final ingredients = await _getIngredients();
     final alcoholPresence = await _getAlcoholic();
 
-    return FillingFilterEntity(
+    return FilterDataEntity(
       categories: categories,
       ingredients: ingredients,
       alcoholPresence: alcoholPresence,
     );
   }
 
-  Future<List<String>> _getCategories() async {
-    final rawList = await _apiService.getList("/list.php?c=list");
+  Future<ApiResponse<T>> _getApiResponse<T extends Serializable>(
+    String path,
+  ) async {
+    final rawList = await _apiService.get(path);
+    return ApiResponse<T>.fromJson(rawList);
+  }
 
-    return rawList.map((e) => CategoryModel.fromJson(e).strCategory).toList();
+  Future<List<String>> _getCategories() async {
+    final apiResponse = await _getApiResponse<CategoryModel>(
+      "/list.php?c=list",
+    );
+    return apiResponse.drinks.map((e) => e.strCategory).toList();
   }
 
   Future<List<String>> _getIngredients() async {
-    final rawList = await _apiService.getList("/list.php?i=list");
-
-    return rawList
-        .map((e) => IngredientModel.fromJson(e).strIngredient)
-        .toList();
+    final apiResponse = await _getApiResponse<IngredientModel>(
+      "/list.php?i=list",
+    );
+    return apiResponse.drinks.map((e) => e.strIngredient).toList();
   }
 
   Future<List<String>> _getAlcoholic() async {
-    final rawList = await _apiService.getList("/list.php?a=list");
-
-    return rawList.map((e) => AlcoholicModel.fromJson(e).strAlcoholic).toList();
+    final apiResponse = await _getApiResponse<AlcoholicModel>(
+      "/list.php?a=list",
+    );
+    return apiResponse.drinks.map((e) => e.strAlcoholic).toList();
   }
 }
