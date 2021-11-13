5314a525c652347f30aa4070c4c4fa0696eaf81b
knospfer <berrueta.mattia@gmail.com>
2021-11-13 15:38:32 +0100

    feat: color palette

---
+++ lib/_core/colors/color_palette.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class ColorPalette {
+  const ColorPalette._();
+
+  static const black = Color(0xff1b1717);
+  static const deepRed = Color(0xff810000);
+  static const lightRed = Color(0xffce1212);
+  static const white = Color(0xffeeebdd);
+}
