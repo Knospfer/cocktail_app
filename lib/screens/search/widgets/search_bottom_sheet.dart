2adad43743263c4bea9b9f6bfa89d95ce24eeeab
knospfer <berrueta.mattia@gmail.com>
2021-11-13 14:42:48 +0100

    feat: horizontal sliver list

--- lib/screens/search/widgets/search_bottom_sheet.dart
+++ lib/screens/search/widgets/search_bottom_sheet.dart
@@ -2,6 +2,7 @@ import 'package:cocktail_app/_core/enums/alcohol_presence.dart';
 import 'package:cocktail_app/_domain/filter/filter_entities.dart';
 import 'package:cocktail_app/_shared/widgets/utility/paddings.dart';
 import 'package:cocktail_app/screens/search/widgets/alcohol_presence_radio_group/alcohol_presence_radio_group.dart';
+import 'package:cocktail_app/screens/search/widgets/horizontal_sliver_list.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -57,19 +58,7 @@ class _SearchBottomSheetState extends State<SearchBottomSheet> {
             ),
           ),
         ),
-        SliverToBoxAdapter(
-          child: SizedBox(
-            height: 30,
-            child: ListView(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              physics: const BouncingScrollPhysics(),
-              scrollDirection: Axis.horizontal,
-              children: widget.filter.categories
-                  .map((e) => Chip(label: Text(e)))
-                  .toList(),
-            ),
-          ),
-        ),
+        SliverChipGridHorizontal(items: widget.filter.categories),
         const SliverPadding(padding: EdgeInsets.only(bottom: 20)),
         const SliverToBoxAdapter(
           child: Padding(
@@ -83,19 +72,7 @@ class _SearchBottomSheetState extends State<SearchBottomSheet> {
             padding: EdgeInsets.only(left: 16, right: 16, bottom: 10),
           ),
         ),
-        SliverToBoxAdapter(
-          child: SizedBox(
-            height: 30,
-            child: ListView(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              physics: const BouncingScrollPhysics(),
-              scrollDirection: Axis.horizontal,
-              children: widget.filter.ingredients
-                  .map((e) => Chip(label: Text(e)))
-                  .toList(),
-            ),
-          ),
-        ),
+        SliverChipGridHorizontal(items: widget.filter.ingredients, rows: 4),
         const SliverToBoxAdapter(
           child: Padding(
             child: Text(
