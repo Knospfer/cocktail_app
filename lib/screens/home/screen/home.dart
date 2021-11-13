61403b78c91fbf49e271aeb0266fbf3ad496c76c
knospfer <berrueta.mattia@gmail.com>
2021-11-13 13:13:24 +0100

    feat: home opens search bottom screen

--- lib/screens/home/screen/home.dart
+++ lib/screens/home/screen/home.dart
@@ -4,6 +4,8 @@ import 'package:cocktail_app/_shared/widgets/cocktail_card/cocktail_card.dart';
 import 'package:cocktail_app/_shared/widgets/main_screen_scaffold/main_screen_scaffold.dart';
 import 'package:cocktail_app/_shared/widgets/staggered_sliver_list/staggered_sliver_list.dart';
 import 'package:cocktail_app/screens/home/view_model/home_view_model.dart';
+import 'package:cocktail_app/screens/search/builder/search_bottom_sheet_builder.dart';
+import 'package:cocktail_app/screens/search/screen/search_bottom_sheet_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -43,12 +45,20 @@ class _HomeScreenState extends State<HomeScreen> {
     await _fetchCocktails();
   }
 
+  Future _showSearchBottomSheet() async {
+    showModalBottomSheet(
+      backgroundColor: Colors.transparent,
+      context: context,
+      builder: (_) => const SearchBottomSheetBuilder(),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MainScreenScaffold(
       title: "Hey",
       subtitle: "What would you \nlike to drink tonight?",
-      onSearchPressed: (){},
+      onSearchPressed: _showSearchBottomSheet,
       children: [
         Consumer<HomeViewModel>(
           builder: (context, viewModel, child) {
