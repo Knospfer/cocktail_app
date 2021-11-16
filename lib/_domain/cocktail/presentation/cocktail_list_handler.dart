080fe77e1871cea1b6432e05a3a86845f319f20a
knospfer <berrueta.mattia@gmail.com>
2021-11-16 10:56:06 +0100

    feat: cocktail list handler handles loadings

--- lib/_domain/cocktail/presentation/cocktail_list_handler.dart
+++ lib/_domain/cocktail/presentation/cocktail_list_handler.dart
@@ -1,12 +1,16 @@
 import 'package:cocktail_app/_core/routes/routes.dart';
 import 'package:cocktail_app/_domain/cocktail/entity/cocktail_entity.dart';
 import 'package:cocktail_app/_domain/filter/filter_entities.dart';
+import 'package:cocktail_app/_shared/widgets/main_screen_scaffold/utils/utility_methods.dart';
 import 'package:cocktail_app/_shared/widgets/staggered_sliver_list/staggered_sliver_list.dart';
 import 'package:cocktail_app/screens/search/utils/show_search_bottom_sheet.dart';
 import 'package:flutter/material.dart';
 
 ///Use this mixin to handle [StaggeredSliverList]
 ///in screens such as [HomeScreen] and [FavouriteScreen]
+///
+///Remember to add [MainScaffoldViewModel] to ChangeNotifierProvider
+///in [Routes] to handle loadings automatically
 ///
 ///Bind StaggeredSliverList with [key] and call
 ///[conditionalUpdateCardList] to update item list
@@ -16,13 +20,17 @@ import 'package:flutter/material.dart';
 ///and update [_isSearching] status
 mixin CocktailListHandler {
   final key = GlobalKey<StaggeredSliverListState<CocktailEntity>>();
+
   bool _firstCall = true;
   bool _isSearching = false;
 
   bool _shouldUpdateCardList(List<CocktailEntity> cocktails) =>
       (_firstCall && cocktails.isNotEmpty) || (_isSearching && !_firstCall);
 
-  void conditionalUpdateCardList(List<CocktailEntity> cocktails) {
+  void conditionalUpdateCardList(
+    BuildContext context, {
+    required List<CocktailEntity> cocktails,
+  }) {
     if (_shouldUpdateCardList(cocktails) && _isSearching) {
       key.currentState?.emptyList();
     }
@@ -35,13 +43,16 @@ mixin CocktailListHandler {
   }
 
   Future<void> filterCocktails(
-    BuildContext context,
-    Future<void> Function({ApplyingFilterEntity? filter})
+    BuildContext context, {
+    required Future<void> Function({ApplyingFilterEntity? filter})
         searchCocktailCallback,
-  ) async {
+  }) async {
     _isSearching = true;
     final filter = await showSearchBottomSheet(context);
-    return searchCocktailCallback(filter: filter);
+
+    showLoading(context);
+    await searchCocktailCallback(filter: filter);
+    dismissLoading(context);
   }
 
   Future<void> navigateToDetail(
